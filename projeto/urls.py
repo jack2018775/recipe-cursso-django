0f812a944e50bc104e93095d03aedcbed61fda80
jackson <jackduos12@gmail.com>
2023-02-01 15:10:19 -0300

    6. introdução às URLs do Django e Django Views
        Os caminhos nas urls nunca começa com a /
    
        Códigos de status de respostas HTTP
    
            Respostas de informação (100-199),
            Respostas de sucesso (200-299),
            Redirecionamentos (300-399)
            Erros do cliente (400-499)
            Erros do servidor (500-599).

--- projeto/urls.py
+++ projeto/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+def my_view(self):
+    return HttpResponse('Test de uma response http')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', my_view )
 ]
